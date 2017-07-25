a54ddf0f1685e05cef91b3dc7668f097f736abeb
Stephen Brown <steve@fig14.com>
2017-07-25 20:07:35 +0100

    Missing .

--- easyzebra/__init__.py
+++ easyzebra/__init__.py
@@ -2,7 +2,7 @@
 Python Zebra ZPL II Library
 """
 
-from driver import Zebra, ZebraBitmap  # noqa
+from .driver import Zebra, ZebraBitmap  # noqa
 
 __author__ = 'Stephen Brown (Little Fish Solutions LTD)'
 
